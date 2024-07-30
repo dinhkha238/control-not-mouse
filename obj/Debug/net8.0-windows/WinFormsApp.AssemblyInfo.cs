78b602231e7b8ff2fc00f3f0abf19d5afb7a598f
kha <nguyendinhkha.s4c.@gmail.com>
2024-07-30 15:32:51 +0700

    final 3

--- obj/Debug/net8.0-windows/WinFormsApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WinFormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b506d5660f96102d7ab19bacbd31c01786e57a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d75f2895001186dd2ac8f18442f51b7ad11aa2e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
