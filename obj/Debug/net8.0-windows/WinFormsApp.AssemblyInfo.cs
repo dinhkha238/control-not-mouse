466efc3016f74358c8f0626251e53ff18fb634e3
kha <nguyendinhkha.s4c.@gmail.com>
2024-07-30 14:49:25 +0700

    sssds

--- obj/Debug/net8.0-windows/WinFormsApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WinFormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a9a683fd2ddbb680c4616fe7672f7a676fa3bb5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+781056b9d69bc526f8f422c8c3d475ea88389943")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
